73af6bc485554483cb4cbd5b3caaaa2ea8da30bc
Gabriel Lagleva <130601518+gabriellagleva@users.noreply.github.com>
2026-05-31 19:15:43 -0400

    qwerqwer

--- ContosoPizza/Pages/PizzaList.cshtml.cs
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -9,8 +9,8 @@ namespace ContosoPizza.Pages
     public class PizzaListModel : PageModel
     {
         private readonly PizzaService _service;
-        public IList<Pizza> PizzaList {get;set;} = default!;
-        
+        public IList<Pizza> PizzaList { get; set; } = default!;
+
         public PizzaListModel(PizzaService service)
         {
             _service = service;
@@ -20,6 +20,27 @@ namespace ContosoPizza.Pages
         {
             PizzaList = _service.GetPizzas();
         }
-        
+
+        public IActionResult OnPostDelete(int id)
+        {
+            _service.DeletePizza(id);
+
+            return RedirectToPage();
+        }
+
+        public Pizza NewPizza { get; set; } = default!;
+
+        public IActionResult OnPost(Pizza newPizza)
+        {
+            if (!ModelState.IsValid || NewPizza == null)
+            {
+                PizzaList = _service.GetPizzas();
+                return Page();
+            }
+
+            _service.AddPizza(NewPizza);
+
+            return RedirectToPage();
+        }
     }
 }
